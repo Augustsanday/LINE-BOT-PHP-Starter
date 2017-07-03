e9092900e8223875875489e77f1df599fa36586d
Augustsanday <onisuka_champion@hotmail.com>
2017-07-03 20:08:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,114 +1,47 @@
 <?php
-$access_token = 'BkXbmWRqZjSYsc638fvlBCFCz/X+g9DrjE5eaME6PcxdrteIqg9grMQlULH5AotuXqWAAAAmeyt7X6Tzh4eiXQmRrWMGe7h3IN7P2P1t+zj59Hj/K0aeGMr7DukG3FSCaS/oqcfCZLZUn+SNLOdvdAdB04t89/1O/w1cDnyilFU=';
-include ('vendor/autoload.php');
-use \LINE\LINEBot;
-use \LINE\LINEBot\HTTPClient;
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot\MessageBuilder;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-class BOT_API extends LINEBot {
-	
-    /* ====================================================================================
-     * Variable
-     * ==================================================================================== */
-	
-    private $httpClient     = null;
-    private $endpointBase   = null;
-    private $channelSecret  = null;
-	
-    public $content         = null;
-    public $events          = null;
-	
-    public $isEvents        = false;
-    public $isText          = false;
-    public $isImage         = false;
-    public $isSticker       = false;
-	
-    public $text            = null;
-    public $replyToken      = null;
-    public $source          = null;
-    public $message         = null;
-    public $timestamp       = null;
-	
-    public $response        = null;
-	
-    /* ====================================================================================
-     * Custom
-     * ==================================================================================== */
-	
-    public function __construct ($channelSecret, $access_token) {
-		
-        $this->httpClient     = new CurlHTTPClient($access_token);
-        $this->channelSecret  = $channelSecret;
-        $this->endpointBase   = LINEBot::DEFAULT_ENDPOINT_BASE;
-		
-        $this->content        = file_get_contents('php://input');
-        $events               = json_decode($this->content, true);
-		
-        if (!empty($events['events'])) {
-			
-            $this->isEvents = true;
-            $this->events   = $events['events'];
-			
-            foreach ($events['events'] as $event) {
-				
-                $this->replyToken = $event['replyToken'];
-                $this->source     = (object) $event['source'];
-                $this->message    = (object) $event['message'];
-                $this->timestamp  = $event['timestamp'];
-				
-                if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-                    $this->isText = true;
-                    $this->text   = $event['message']['text'];
-                }
-				
-                if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
-                    $this->isImage = true;
-                }
-				
-                if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
-                    $this->isSticker = true;
-                }
-				
-            }
-        }
-		
-        parent::__construct($this->httpClient, [ 'channelSecret' => $channelSecret ]);
-		
-    }
-	
-    public function sendMessageNew ($to = null, $message = null) {
-        $messageBuilder = new TextMessageBuilder($message);
-        $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/push', [
-            'to' => $to,
-            // 'toChannel' => 'Channel ID,
-            'messages'  => $messageBuilder->buildMessage()
-        ]);
-    }
-	
-    public function replyMessageNew ($replyToken = null, $message = null) {
-        $messageBuilder = new TextMessageBuilder($message);
-        $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
-            'replyToken' => $replyToken,
-            'messages'   => $messageBuilder->buildMessage(),
-        ]);
-    }
-	
-    public function isSuccess () {
-        return !empty($this->response->isSucceeded()) ? true : false;
-    }
-	
-    public static function verify ($access_token) {
-		
-        $ch = curl_init('https://api.line.me/v1/oauth/verify');
-		
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [ 'Authorization: Bearer ' . $access_token ]);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return json_decode($result);
-		
-    }
-	
+$strAccessToken = "BkXbmWRqZjSYsc638fvlBCFCz/X+g9DrjE5eaME6PcxdrteIqg9grMQlULH5AotuXqWAAAAmeyt7X6Tzh4eiXQmRrWMGe7h3IN7P2P1t+zj59Hj/K0aeGMr7DukG3FSCaS/oqcfCZLZUn+SNLOdvdAdB04t89/1O/w1cDnyilFU=";
+ 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+}else{
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+?>
