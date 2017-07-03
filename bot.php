1b80e129431564b35e0780a44a66bbb29b81011a
Augustsanday <onisuka_champion@hotmail.com>
2017-07-03 20:04:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,5 @@
 <?php
 $access_token = 'BkXbmWRqZjSYsc638fvlBCFCz/X+g9DrjE5eaME6PcxdrteIqg9grMQlULH5AotuXqWAAAAmeyt7X6Tzh4eiXQmRrWMGe7h3IN7P2P1t+zj59Hj/K0aeGMr7DukG3FSCaS/oqcfCZLZUn+SNLOdvdAdB04t89/1O/w1cDnyilFU=';
-
-<?php
 include ('vendor/autoload.php');
 use \LINE\LINEBot;
 use \LINE\LINEBot\HTTPClient;
