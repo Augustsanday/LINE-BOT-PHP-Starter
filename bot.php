6807a607f1416ccab447c19a1960be78675a5784
Augustsanday <onisuka_champion@hotmail.com>
2017-07-03 15:22:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -45,3 +45,16 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK"; 
+//
+TextMessage textMessage = new TextMessage("hello");
+ReplyMessage replyMessage = new ReplyMessage(
+        "<replyToken>",
+        textMessage
+);
+Response<BotApiResponse> response =
+        LineMessagingServiceBuilder
+                .create("<channel access token>")
+                .build()
+                .replyMessage(replyMessage)
+                .execute();
+System.out.println(response.code() + " " + response.message());
